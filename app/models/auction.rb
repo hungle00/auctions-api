1ac99b2ae74f3afc6b1c1cf4d6d74ab3ffa0bf61
hungle00 <hungle2048@gmail.com>
2021-01-18 15:02:04 +0700

    Add auction, bid controller

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,5 +1,5 @@
 class Auction < ApplicationRecord
   belongs_to :user, optional: true
-  has_many :bids
+  has_many :bids, dependent: :destroy
   validates_presence_of :title, :description, :start_date, :end_date
 end
